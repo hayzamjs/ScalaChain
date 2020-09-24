4215263ff024dcbbd0d9a1e0329d69ba78f2a5ab
hayzamjs <hayzam@gmail.com>
2020-09-24 07:31:00 +0530

    Completed refactor
    Adds support for the onion-monero based api

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-module.exports = { EXPLORER_API: "http://192.168.1.8:8081",
-                   NODE_ADDRESS: "http://192.168.1.8:11812" };
+module.exports = {
+  EXPLORER_API: 'http://192.168.1.8:8081',
+  NODE_ADDRESS: 'http://192.168.1.8:11812'
+}
--- index.js
+++ index.js
@@ -1,85 +1,296 @@
 /* Express and Templating */
 const express = require('express')
 const app = express()
-const port = 1932;
+const port = 1932
+const path = require('path')
+const bodyParser = require('body-parser')
 const mustacheExpress = require('mustache-express')
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json())
 app.engine('html', mustacheExpress())
 app.set('view engine', 'html')
-app.set('views', __dirname + '/public')
+app.set('views', path.join(__dirname, '/public'))
 app.use(express.static('public'))
 
 /* Requests */
 const bent = require('bent')
-const getJSON = bent('json')
 
 /* Configurations */
 
-const config = require("./config")
-const explorerAPI = config.EXPLORER_API;
-const nodeAddress = config.NODE_ADDRESS;
+const config = require('./config')
+const explorerAPI = config.EXPLORER_API
+const nodeAddress = config.NODE_ADDRESS
 
-const GET_EXPLORER_API = bent(explorerAPI, 'GET', 'json', 200);
-const POST_NODE_API = bent(nodeAddress, 'POST', 'json', 200);
+const GET_EXPLORER_API = bent(explorerAPI, 'GET', 'json', 200)
+const POST_NODE_API = bent(nodeAddress, 'POST', 'json', 200)
 
-async function getBlock(heightOrHash){
-    if(isNaN(heightOrHash) == true){
-        return await POST_NODE_API('/json_rpc', {"jsonrpc":"2.0","id":"0","method":"get_block","params":{"hash":heightOrHash}});
-    }else{
-        return await POST_NODE_API('/json_rpc', {"jsonrpc":"2.0","id":"0","method":"get_block","params":{"height":heightOrHash}});
-    }
+async function getBlock (heightOrHash) {
+  if (isNaN(heightOrHash) === true) {
+    return await POST_NODE_API('/json_rpc', { jsonrpc: '2.0', id: '0', method: 'get_block', params: { hash: heightOrHash } })
+  } else {
+    return await POST_NODE_API('/json_rpc', { jsonrpc: '2.0', id: '0', method: 'get_block', params: { height: heightOrHash } })
+  }
 }
 
-app.get('/', async (req, res) => {
-    /* Network info */
-    const networkInfo = await GET_EXPLORER_API('/api/networkinfo');
+async function getBlockRange (startHeight, endHeight) {
+  return await POST_NODE_API('/json_rpc', { jsonrpc: '2.0', id: '0', method: 'get_block_headers_range', params: { start_height: startHeight, end_height: endHeight } })
+}
 
-    /* Transactions */
-    const memPool = await GET_EXPLORER_API('/api/mempool');
-    const memPoolTxs = memPool.data.txs;
-    const memPoolTxCount = memPool.data.txs_no;
-    let sizeOfPool = 0;
-    const totalCount = networkInfo.data.tx_count;
+async function getNetworkInfo () {
+  return await GET_EXPLORER_API('/api/networkinfo')
+}
 
-    if(memPoolTxCount != 0){
-            memPoolTxs.forEach(tx => {
-                  sizeOfPool += tx.tx_size;
-            });
-            sizeOfPool = sizeOfPool / 1000; /* To get it in kilobytes */
+async function getTransaction (txHash) {
+  return await GET_EXPLORER_API(`/api/transaction/${txHash}`)
+}
+
+async function proveTransacion (txHash, address, key, method) {
+  const proof = await GET_EXPLORER_API(`/api/outputs?txhash=${txHash}&address=${address}&viewkey=${key}&txprove=${method}`)
+  if (proof.status !== 'error') {
+    if (proof.data.tx_prove === true) {
+      return true
     }
+    return false
+  }
+  return false
+}
 
-    /* Last block info */
+async function isBlock (data) {
+  const getBlock = await POST_NODE_API('/json_rpc', { jsonrpc: '2.0', id: '0', method: 'get_block', params: { hash: data } })
+  if (getBlock.error) {
+    return false
+  } else {
+    return true
+  }
+}
 
-    const lastHeight = networkInfo.data.height - 1
-    const lastBlockInfo = await getBlock(lastHeight)
-    console.log(lastBlockInfo)
+async function getMempoolSize () {
+  const memPool = await GET_EXPLORER_API('/api/mempool')
+  const memPoolTxs = memPool.data.txs
+  const memPoolTxCount = memPool.data.txs_no
+  let sizeOfPool = 0
 
-    res.render("explorer",
-                {
-                    "block_height": networkInfo.data.height,
-                    "difficulty_current":networkInfo.data.difficulty,
-                    "hashrate_current":(((networkInfo.data.difficulty) / 120) / 1000000).toFixed(2),
-                    "pool_tx_count":memPoolTxCount,
-                    "tx_pool_data_size":(sizeOfPool).toFixed(2),
-                    "transaction_count":totalCount,
-                    "incoming_conn_count":networkInfo.data.incoming_connections_count,
-                    "outgoing_conn_count":networkInfo.data.outgoing_connections_count,
-                    "last_hash_top":lastBlockInfo.result.block_header.hash,
-                    "last_block_height_top":lastBlockInfo.result.block_header.height,
-                    "last_block_reward":lastBlockInfo.result.block_header.reward,
-                    "last_block_difficulty":lastBlockInfo.result.block_header.difficulty,
-                    "last_block_when":lastBlockInfo.result.block_header.timestamp,
-                    "last_block_txs_count":lastBlockInfo.result.block_header.num_txes
-                });
-});
+  if (memPoolTxCount !== 0) {
+    memPoolTxs.forEach(tx => {
+      sizeOfPool += tx.tx_size
+    })
+    return (sizeOfPool / 1000) /* To get it in kilobytes */
+  } else {
+    return 0
+  }
+}
 
-app.get('/go', async (req, res) => {
+app.get('/prove', async (req, res) => {
+  res.render('prove')
+})
 
-});
+app.post('/prove', async (req, res) => {
+  const proof = await proveTransacion(req.body.tx_hash, req.body.key, req.body.address, req.body.method)
+  if (proof === true) {
+    res.send(true)
+  } else {
+    res.send(false)
+  }
+})
 
-app.get('/tx', function(req, res) {
-});
+app.get('/', async (req, res) => {
+  /* Network info */
+  const networkInfo = await getNetworkInfo()
 
-app.get('/block', function(req, res) {
-});
+  /* Transactions */
+  const sizeOfPool = await getMempoolSize()
 
-app.listen(port, () => console.log(`ScalaChain app listening on port ${port}!`));
+  /* Last block info */
+  const lastHeight = networkInfo.data.height - 1
+  const lastBlockInfo = await getBlock(lastHeight)
+  const lastBlockTxHashes = lastBlockInfo.result.tx_hashes
+  const transactionsCount = lastBlockInfo.result.block_header.num_txes
+
+  /* Transactions inside the last block */
+  let transactionsHTML = ''
+
+  if (transactionsCount > 0) {
+    transactionsHTML = `<thead>
+        <tr>
+            <th>Transaction hash</th>
+            <th>Unlock Block</th>
+            <th class='t-right'>Status</th>
+        </tr>
+        </thead>
+        <tbody>`
+    lastBlockTxHashes.forEach(txHash => {
+      transactionsHTML += `<tr>
+                                     <td>${txHash}</td>
+                                     <td>${lastHeight + 10}</td>
+                                     <td class='t-right c-green'>
+                                        <i class='fas fa-check no-margin'></i>
+                                    </td>
+                                  </tr>`
+    })
+    transactionsHTML += '</tbody>'
+  } else {
+    transactionsHTML = '<thead><tr><th>No Transactions in the last block</th></tr></thead>'
+  }
+
+  /* Previous 10 blocks */
+
+  const tenBlocks = await getBlockRange((lastHeight - 11), (lastHeight - 1))
+  const tenBlocksHeaders = (tenBlocks.result.headers).reverse()
+  let tenBlocksHTML = '<tbody id=\'txBody2\'>'
+  tenBlocksHeaders.forEach(header => {
+    const headerLink = `/block?block_info=${header.hash}`
+    tenBlocksHTML += `<tr><td><a href="${headerLink}">${header.hash.substring(0, 5)}...${header.hash.substring(header.hash.length - 5)}</a></td><td>${header.height}</td><td>${(header.reward) / 100}</td>
+        <td>${(header.difficulty)}</td><td>${(header.timestamp)}</td><td class='t-right'>${(header.num_txes)}</td></tr>`
+  })
+  tenBlocksHTML += '</tbody>'
+
+  res.render('explorer',
+    {
+      block_height: networkInfo.data.height,
+      difficulty_current: networkInfo.data.difficulty,
+      hashrate_current: (((networkInfo.data.difficulty) / 120) / 1000000).toFixed(2),
+      pool_tx_count: networkInfo.data.tx_pool_size,
+      tx_pool_data_size: (sizeOfPool).toFixed(2),
+      transaction_count: networkInfo.data.tx_count,
+      incoming_conn_count: networkInfo.data.incoming_connections_count,
+      outgoing_conn_count: networkInfo.data.outgoing_connections_count,
+      last_hash_top: lastBlockInfo.result.block_header.hash,
+      last_block_height_top: lastBlockInfo.result.block_header.height,
+      last_block_reward: ((lastBlockInfo.result.block_header.reward) / 100),
+      last_block_difficulty: lastBlockInfo.result.block_header.difficulty,
+      last_block_when: lastBlockInfo.result.block_header.timestamp,
+      last_block_txs_count: transactionsCount,
+      prev_blocks_html: tenBlocksHTML,
+      last_blocks_transactions: transactionsHTML
+    })
+})
+
+app.get('/tx', async (req, res) => {
+  const networkInfo = await getNetworkInfo()
+  const txInfo = await getTransaction(req.query.tx_info)
+  const confirmed = ((txInfo.data.current_height - txInfo.data.block_height) > 10)
+  let txStatus = ''
+  let paymentId = ''
+  const sizeOfPool = await getMempoolSize()
+
+  if (confirmed) {
+    txStatus += '<i class=\'fa fa-check no-margin\'></i>'
+  } else {
+    txStatus += '<i class=\'fa fa-clock no-margin\'></i>'
+  }
+
+  if (txInfo.data.payment_id !== '' || txInfo.data.payment_id8 !== '') {
+    paymentId += '<i class="fa fa-times" aria-hidden="true"></i>'
+  } else {
+    if (txInfo.data.payment_id !== '') {
+      paymentId += txInfo.data.payment_id
+    } else {
+      paymentId += txInfo.data.payment_id8
+    }
+  }
+
+  let keyImagesInputHTML = ''
+  let keyImagesOutputHTML = '';
+
+  (txInfo.data.inputs).forEach(input => {
+    keyImagesInputHTML += `<tr><td>?</td><td>${input.key_image}</td></tr>`
+  });
+
+  (txInfo.data.outputs).forEach(output => {
+    keyImagesOutputHTML += `<tr><td>?</td><td>${output.public_key}</td></tr>`
+  })
+
+  res.render('go_tx',
+    {
+      tx_hash: txInfo.data.tx_hash,
+      mixins: txInfo.data.mixin,
+      tx_status: txStatus,
+      tx_extra: txInfo.data.extra,
+      payment_id: paymentId,
+      rct_type: txInfo.data.rct_type,
+      timestamp: txInfo.data.timestamp,
+      timestamp_utc: txInfo.data.timestamp_utc,
+      tx_fee: `${(txInfo.data.tx_fee / 100)} XLA`,
+      tx_size: `${(txInfo.data.tx_size / 1000).toFixed(2)} kb`,
+      tx_version: txInfo.data.tx_version,
+      key_image_inputs: keyImagesInputHTML,
+      key_image_outputs: keyImagesOutputHTML,
+      stat_block_height: networkInfo.data.height,
+      stat_block_difficulty: networkInfo.data.difficulty,
+      stat_block_hashrate: (((networkInfo.data.difficulty) / 120) / 1000000).toFixed(2),
+      stat_txcount: networkInfo.data.tx_count,
+      stat_tx_pool_size: networkInfo.data.tx_pool_size,
+      tx_pool_data_size: sizeOfPool,
+      stat_incom: networkInfo.data.incoming_connections_count,
+      stat_outgo: networkInfo.data.outgoing_connections_count
+    })
+})
+
+app.get('/search', async (req, res) => {
+  const vin = await isBlock(req.query.data)
+  if (vin === true) {
+    res.redirect(`/block?block_info=${req.query.data}`)
+  } else {
+    res.redirect(`/tx?tx_info=${req.query.data}`)
+  }
+})
+
+app.get('/block', async (req, res) => {
+  const gotBlockInfo = req.query.block_info
+  const blockInfo = await getBlock(gotBlockInfo)
+  const lastBlockTxHashes = blockInfo.result.tx_hashes
+  const transactionsCount = blockInfo.result.block_header.num_txes
+  const networkInfo = await getNetworkInfo()
+  const sizeOfPool = await getMempoolSize()
+
+  let blockTxsHTML = ''
+  if (transactionsCount > 0) {
+    blockTxsHTML += `<thead>
+        <tr>
+            <th>Transaction hash</th>
+            <th>Unlock Block</th>
+            <th class='t-right'>Status</th>
+        </tr>
+        </thead>
+        <tbody>`
+
+    lastBlockTxHashes.forEach(txHash => {
+      blockTxsHTML += `<tr>
+                                     <td>${txHash}</td>
+                                     <td>${blockInfo.result.block_header.height + 10}</td>
+                                     <td><td class='t-right c-green'>
+                                        <i class='fas fa-check no-margin'></i>
+                                    </td>
+                                  </tr>`
+    })
+
+    blockTxsHTML += '</tbody>'
+  } else {
+    blockTxsHTML = '<thead><tr><th>No Transactions in this block</th></tr></thead>'
+  }
+
+  res.render('go_block',
+    {
+      block_hash: blockInfo.result.block_header.hash,
+      block_height: blockInfo.result.block_header.height,
+      block_reward: ((blockInfo.result.block_header.reward) / 100),
+      difficulty: blockInfo.result.block_header.difficulty,
+      time_ago: blockInfo.result.block_header.timestamp,
+      time_stamp: blockInfo.result.block_header.timestamp,
+      tx_count: transactionsCount,
+      inblock_txs: blockTxsHTML,
+      stat_block_height: networkInfo.data.height,
+      stat_block_difficulty: networkInfo.data.difficulty,
+      stat_block_hashrate: (((networkInfo.data.difficulty) / 120) / 1000000).toFixed(2),
+      stat_txcount: networkInfo.data.tx_count,
+      stat_tx_pool_size: networkInfo.data.tx_pool_size,
+      tx_pool_data_size: sizeOfPool,
+      stat_incom: networkInfo.data.incoming_connections_count,
+      stat_outgo: networkInfo.data.outgoing_connections_count
+    })
+})
+
+app.listen(port, () => console.log(`ScalaChain app listening on port ${port}!`))
